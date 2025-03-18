a0b71e2c80b8bd453a2ccbfb7578874bb792bc21
tkaiwann <atom.tichila@gmail.com>
2025-03-18 15:48:38 +0700

    Fix duplicates

--- cell02/ex02/password.py
+++ cell02/ex02/password.py
@@ -3,9 +3,3 @@ password = "Python is awesome"
 input_password = input("Please enter password : ")
 if input_password == password :  print("ACCESS GRANTED")
 else : print("ACCESS DENIED")
-
-#!/usr/bin/env python3
-password = "Python is awesome"
-input_password = input("Please enter password : ")
-if input_password == password :  print("ACCESS GRANTED")
-else : print("ACCESS DENIED")
