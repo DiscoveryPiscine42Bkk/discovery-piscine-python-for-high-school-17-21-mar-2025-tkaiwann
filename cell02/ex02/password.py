dbda08f6720385921b74cb609e4ffbaae374c9e1
tkaiwann <atom.tichila@gmail.com>
2025-03-18 11:52:52 +0700

    Add files via upload

---
+++ cell02/ex02/password.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+password = "Python is awesome"
+input_password = input("Please enter password : ")
+if input_password == password :  print("ACCESS GRANTED")
+else : print("ACCESS DENIED")
